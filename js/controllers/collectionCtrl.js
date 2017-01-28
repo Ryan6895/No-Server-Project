733c2a8eeade3e1a2b54bbae3a356e6eb8fe2e0b
Ryan Clark <Rymaster95@gmail.com>
2017-01-28 10:57:22 -0700

    almost final product

--- js/controllers/collectionCtrl.js
+++ js/controllers/collectionCtrl.js
@@ -1,5 +1,5 @@
 angular.module('App')
-.controller('collectionCtrl', function($scope, listService) {
+.controller('collectionCtrl', function($scope,service, listService) {
   $scope.movies = listService.collections;
   $scope.movieImg = "https://image.tmdb.org/t/p/original";
 
@@ -9,6 +9,9 @@ angular.module('App')
     $scope.active3 = ''
     $scope.thisList = "owned"
     $scope.listTitle = 'Owned'
+    $scope.moveItem1 = 'moveItemActive'
+    $scope.moveItem2 = ''
+    $scope.smaller = 'collection_ListHeight'
   }
   $scope.ownList();
   $scope.wantList = function(){
@@ -17,6 +20,9 @@ angular.module('App')
     $scope.active3 = ''
     $scope.thisList = "want"
     $scope.listTitle = 'Want'
+    $scope.moveItem1 = ''
+    $scope.moveItem2 = 'moveItemActive'
+    $scope.smaller = 'collection_ListHeight'
   }
   $scope.upcomingList = function(){
     $scope.active3 = 'mycollectionActive'
@@ -24,8 +30,16 @@ angular.module('App')
     $scope.active1 = ''
     $scope.thisList = "upcoming"
     $scope.listTitle = 'Upcoming'
+    $scope.moveItem1 = ''
+    $scope.moveItem2 = ''
+    $scope.smaller = ''
   }
   $scope.removeItem = function(movie,id) {
     listService.removeItemFromList(movie,id);
   }
+  $scope.moveMovie = function(list, movie){
+    $scope.hovering = false;
+    listService.addItemToList(list, movie)
+  }
+
 })
